cdbeb5649b9d752693688ac9d2d296c386b8cd65
Batuhan ÇAĞAN <65660059+BatuhanCagan@users.noreply.github.com>
2022-06-29 00:28:45 +0300

    Add files via upload

--- repo/main.py
+++ repo/main.py
@@ -1,11 +1,17 @@
+import os
 import numpy as np
 import cv2
 from numpy import asarray
+import time
 
-outFile = "frames.npy"
-videoLocations = input()
+vids = os.listdir()
+get = 'mp4 MOV avi'
+funcVids = list(filter(lambda a: any([1 if get in a else 0 for get in get.split(' ')]), vids))
+#videoLocations = input()
+
 
 def saveFrames(videos):
+    outFile = "frames-"+"-{}-".format(time.localtime())+".npy"
     video = cv2.VideoCapture(videos)
     fps = video.get(cv2.CAP_PROP_FPS)
     success, image = video.read()
@@ -16,9 +22,9 @@ def saveFrames(videos):
         print(gray)
         success, image = video.read()
         count += 1
-    print("1", int(fps), "Total Frames:", count,np.shape(data),"1")
+    print("1", int(fps), "Total Frames:", count, np.shape(data), "1")
     print(data)
     np.save(outFile, data)
 
 
-saveFrames(videoLocations)
+a = map(saveFrames, funcVids)
