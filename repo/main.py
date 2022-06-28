3a28b240fa9ddd195e91cfbeca21af6c687c0296
Batuhan ÇAĞAN <65660059+BatuhanCagan@users.noreply.github.com>
2022-06-28 20:29:40 +0300

    Add files via upload

--- repo/main.py
+++ repo/main.py
@@ -1,3 +1,24 @@
-videoLocations = list(input())
+import numpy as np
+import cv2
+from numpy import asarray
 
-def saveFrames(videoLocations):
+outFile = "frames.npy"
+videoLocations = input()
+
+def saveFrames(videos):
+    video = cv2.VideoCapture(videos)
+    fps = video.get(cv2.CAP_PROP_FPS)
+    success, image = video.read()
+    count = 1
+    while success:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        data = asarray(gray)
+        print(gray)
+        success, image = video.read()
+        count += 1
+    print("1", int(fps), "Total Frames:", count,np.shape(data),"1")
+    print(data)
+    np.save(outFile, data)
+
+
+saveFrames(videoLocations)
