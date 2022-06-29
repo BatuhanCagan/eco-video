c45e163867c52dbb4dde104efe1e18b914d063dc
Batuhan ÇAĞAN <65660059+BatuhanCagan@users.noreply.github.com>
2022-06-29 14:13:21 +0300

    Add files via upload

--- repo/main.py
+++ repo/main.py
@@ -1,30 +1,23 @@
 import os
 import numpy as np
 import cv2
-from numpy import asarray
-import time
-
-vids = os.listdir()
-get = 'mp4 MOV avi'
-funcVids = list(filter(lambda a: any([1 if get in a else 0 for get in get.split(' ')]), vids))
-#videoLocations = input()
 
 
-def saveFrames(videos):
-    outFile = "frames-"+"-{}-".format(time.localtime())+".npy"
-    video = cv2.VideoCapture(videos)
-    fps = video.get(cv2.CAP_PROP_FPS)
-    success, image = video.read()
-    count = 1
-    while success:
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        data = asarray(gray)
-        print(gray)
+def saveFrames():
+    vids = os.listdir()
+    get = 'mp4 MOV avi'
+    funcVids = list(filter(lambda a: any([1 if get in a else 0 for get in get.split(' ')]), vids))
+    outFile = "frames.npy"
+    videoList = []
+    for videos in funcVids:
+        video = cv2.VideoCapture(videos)
         success, image = video.read()
-        count += 1
-    print("1", int(fps), "Total Frames:", count, np.shape(data), "1")
-    print(data)
-    np.save(outFile, data)
+        while success:
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            videoList.append(gray)
+            success, image = video.read()
+    videoList = np.array(videoList).reshape(-1, gray.shape[0], gray.shape[1], 1)
+    print(videoList.shape)
+    np.save(outFile, videoList)
 
-
-a = map(saveFrames, funcVids)
+saveFrames()
